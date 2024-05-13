8c2b441678d54ebafde8291059095d4c5d11ff26
NathanBergmann <nathan.bergmann@agapys.com>
2024-05-13 13:11:20 -0300

    Add Aba Destinos
    * Adicionado Rota /destinos
    * Falta o link do forms para cadastrar o destino

--- fso-turmaa-proj-main/fso-turmaa-proj-main/projfso2024a/src/main/java/br/univille/projfso2024a/controller/DestinoController.java
+++ fso-turmaa-proj-main/fso-turmaa-proj-main/projfso2024a/src/main/java/br/univille/projfso2024a/controller/DestinoController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.univille.projfso2024a.entity.Destino;
+
 @Controller
 @RequestMapping("/destinos")
 public class DestinoController {
@@ -12,5 +14,11 @@ public class DestinoController {
     @GetMapping
     public ModelAndView index(){
         return new ModelAndView("destino/index");
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView novo(){
+        var destino = new Destino();
+        return new ModelAndView("destino/form","destino",destino);
     }
 }
